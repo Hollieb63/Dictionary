665ac3386ace338e715a4393e23a121ec47dd8e1
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2024-04-24 17:16:15 +0100

    created dictionary js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     
+     <div className="container">
+     <main>
+      <Dictionary/>
+     </main>
+
+      <footer className="text-center">Coded by Hollie Bradshaw</footer>
+     </div>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/Dictionary.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Dictionary(){
+    return(
+        <div className="Dictionary">
+            hello
+        </div>
+    )
+}
